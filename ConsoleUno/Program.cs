d2537bbbc0636452c898fcc266a0c61a5489865c
Klaus Thevessen <flm@thevessen.ch>
2020-10-01 06:08:25 +0200

    test trigger 2

--- ConsoleUno/Program.cs
+++ ConsoleUno/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleUno
         static void Main(string[] args)
         {
             Console.WriteLine("programm UNO - qa_branch2");
-            Console.WriteLine("test VCS-trigger  - nur bei 'qa_branch2");
+            Console.WriteLine("test VCS-trigger  - NUR bei 'qa_branch2");
             Console.ReadKey();
         }
     }
